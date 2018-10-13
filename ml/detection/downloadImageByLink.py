489c68ec844c57e7355d992f66cce46d34f1956f
Sebastian <anse23@hotmail.com>
2018-10-13 16:10:21 -0500

    feat: Updated script to use arguments

--- ml/detection/downloadImageByLink.py
+++ ml/detection/downloadImageByLink.py
@@ -2,26 +2,36 @@ import urllib.request
 import cv2
 import numpy as np
 import os
+import argparse
 
-def store_raw_images():
-    neg_images_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07942152'
+def store_raw_images(directory, url):
+    neg_images_link = url
     neg_image_urls = urllib.request.urlopen(neg_images_link).read().decode()
     pic_num = 1
 
-    if not os.path.exists('neg'):
-        os.makedirs('neg')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
     for i in neg_image_urls.split('\n'):
         try:
             print(i)
-            urllib.request.urlretrieve(i, "neg/"+str(pic_num)+".jpg")
-            img = cv2.imread("neg/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
+            im_name = "{:s}/{:d}.jpg".format(directory, pic_num)
+            urllib.request.urlretrieve(i, im_name)
+            img = cv2.imread(im_name, cv2.IMREAD_GRAYSCALE)
             # should be larger than samples / pos pic (so we can place our image on it)
             resized_image = cv2.resize(img, (100, 100))
-            cv2.imwrite("neg/"+str(pic_num)+".jpg",resized_image)
+            cv2.imwrite(im_name, resized_image)
             pic_num += 1
 
         except Exception as e:
             print(str(e))
 
-store_raw_images()
+parser = argparse.ArgumentParser(description="Download images")
+parser.add_argument('directory', type=str)
+parser.add_argument('url', type=str)
+
+args = parser.parse_args()
+directory = args.directory
+url = args.url
+
+store_raw_images(directory, url)
