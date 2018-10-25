0b7f9192be02cda740ac973d62ceef8d3a29803f
Sebastian <anse23@hotmail.com>
2018-10-25 16:07:41 -0500

    feat: Added script to download watches from ethos

---
+++ ml/detection/downloadImageByLink2.py
@@ -0,0 +1,30 @@
+import urllib.request
+import cv2
+import numpy as np
+import os
+import argparse
+
+def store_raw_images(directory, list_file):
+    neg_image_urls = open(list_file, "r");
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for i in neg_image_urls.read().split('\n'):
+        try:
+            print(i)
+            im_name = "{:s}/{:s}".format(directory, i.split("/")[-1])
+            urllib.request.urlretrieve(i, im_name)
+
+        except Exception as e:
+            print(str(e))
+
+parser = argparse.ArgumentParser(description="Download images")
+parser.add_argument('directory', type=str)
+parser.add_argument('list_file', type=str)
+
+args = parser.parse_args()
+directory = args.directory
+list_file = args.list_file
+
+store_raw_images(directory, list_file)
