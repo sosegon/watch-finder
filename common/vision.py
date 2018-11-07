bb5fa30553628ceb7aa0f6a194d2b003feee788a
Sebastian <anse23@hotmail.com>
2018-11-07 14:21:30 -0500

    feat: Added function to encode/decode in vision module

--- common/vision.py
+++ common/vision.py
@@ -96,3 +96,15 @@ def make_square(img):
             n_img[top:-bottom, :, :] = img
 
     return n_img
+
+def base64_encode_image(a):
+    return base64.b64encode(a).decode('utf-8')
+
+def base64_decode_image(a, dtype, shape):
+    if sys.version_info.major == 3:
+        a = bytes(a, encoding='utf-8')
+
+    a = np.frombuffer(base64.decodestring(a), dtype=dtype)
+    a = a.reshape(shape)
+
+    return a
